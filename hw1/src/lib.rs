d3c5d7f929351ceea1c2dd104d5d2d13ac11f140
Bender <zron25@gmail.com>
2025-09-19 13:05:53 -0400

    The game now runs

--- hw1/src/lib.rs
+++ hw1/src/lib.rs
@@ -41,6 +41,12 @@ pub struct GamePiece
     pub x: usize,
     pub y: usize,
 }
+impl GamePiece {
+    pub fn move_to(&mut self, new_x: usize, new_y: usize) {
+        self.x = new_x;
+        self.y = new_y;
+    }
+}
 
 #[derive(Clone)]
 pub struct Grid {
@@ -49,20 +55,14 @@ pub struct Grid {
     pub walls: Vec<GamePiece>,
     pub minotaur: GamePiece,
     pub theseus: GamePiece,
-
-    // TODO: Implement the Grid struct
+    pub goal: GamePiece,
 }
 impl Grid {
-    fn AddWall(&mut self,WallPiece: GamePiece)
-    {
-        self.walls.push(WallPiece);
-    }
- }
+}
 
 #[derive(Clone)]
-pub struct Game(&self) {
-    grid: Grid,
-    // TODO: Implement the Game struct
+pub struct Game {
+    pub grid: Grid,
 }
 
 // So I need to get the height and width of the maze so the number of rows and columns one then get the x t and m and get their index so like. Get the location of thesues and its x and y position get the location of minotaur and get its x and y postion then make a grid that has like empty space which is any postion that is not covered by the x's
@@ -71,104 +71,194 @@ pub struct Game(&self) {
 // to make an array of this x's
 
 impl Game {
-    // TODO: replace the function body with your implementation
-    pub fn from_board(board: &str) -> Result<Game, BoardError> {
-        
-        println!("{}",board);
-      
-        let lines: Vec<&str> = boards.lines().collect()
-        let numofrows = lens.len();
+    pub fn from_board(board: &str) -> Result<Game, BoardError> {      
+        let lines: Vec<&str> = board.lines().collect();
+        let numofrows = lines.len();
 
         if numofrows == 0 {
             return Err(BoardError::InvalidSize)
         }
         let numofcolumns = lines[0].len();
 
-        let mut walls = Vec::new()
+        let mut wallstoadd = Vec::new();
         let mut theseus_pos: Option<GamePiece> = None;
-        let mut minotaur_pos: Option<GamePiece>= None;
-
+        let mut minotaur_pos: Option<GamePiece> = None;
+        let mut goal_pos: Option<GamePiece> = None;
         for y in 0..numofrows
         {
             for x in 0..numofcolumns
             {
-                let character = lines.[y].chars().nth(x); // I may change this to a match statement
-                if character == 'X'
-                {
-                    walls.push(GamePiece {x,y});
-                }
-                else if character == 'T' {
-                    thesus_pos = Some(GamePiece {x,y});
-                }
-                else if character == 'T' {
-                    minotaur_pos = Some(GamePiece{x, });
-                }
-                else if character == ' ' {
-                    
-                }
-                else
-                {
-                    other => return Err(BoardError::InvalidCharacter(other));
-                }
+                match lines[y].chars().nth(x) {
+                    Some('X') => wallstoadd.push(GamePiece { x, y }),
+                    Some('T') => {
+                        if theseus_pos.is_some() {
+                           return Err(BoardError::MultipleTheseus);
+                        }
+                        theseus_pos = Some(GamePiece { x, y });
+                    }
+                    Some('M') => {
+                    if minotaur_pos.is_some() {
+                        return Err(BoardError::MultipleMinotaur);
+                    }
+                        minotaur_pos = Some(GamePiece { x, y });
+                    }
+                    Some('G') => {
+                        if goal_pos.is_some() {
+                            return Err(BoardError::MultipleGoal);
+                        }
+                    goal_pos = Some(GamePiece{x,y})
+                    }
+                
+                Some(' ') => {},
+                Some(c) => return Err(BoardError::InvalidCharacter(c)), 
+                None => return Err(BoardError::InvalidSize), 
+                }     
             }
         }
 
         let grid = Grid {
             width: numofcolumns,
             height: numofrows,
-            walls: walls,
-            thesus: thesus_pos,
-            minotaur: minotaur_pos,
+            walls: wallstoadd, 
+            theseus: theseus_pos.ok_or(BoardError::NoTheseus)?,
+            minotaur: minotaur_pos.ok_or(BoardError::NoMinotaur)?, 
+            goal: goal_pos.ok_or(BoardError::NoGoal)?,
+        };
+
+        Ok(Game {
+            grid,
+        })
+    }
+
+    pub fn show(&self) {
+        for y in 0..self.grid.height {
+            for x in 0..self.grid.width {
+                if self.is_theseus(y, x) {
+                    print!("T");
+                } else if self.is_minotaur(y, x) {
+                    print!("M");
+                } else if self.is_goal(y, x) {
+                    print!("G");
+                } else if self.is_wall(y, x) {
+                    print!("X");
+                } else {
+                    print!(" ");
+                }
+            }
+            println!();
+        }
+    }
+
+    pub fn minotaur_move(&mut self) {
+        let tx = self.grid.theseus.x;
+        let ty = self.grid.theseus.y;
+        let mx = self.grid.minotaur.x;
+        let my = self.grid.minotaur.y;
+        let dist_x = (tx as isize - mx as isize).abs() as usize;
+        let dist_y = (ty as isize - my as isize).abs() as usize;
+
+        // Check left: if valid and decreases x-distance
+        if mx > 0 && !self.is_wall(my, mx - 1) {
+            let new_mx = mx - 1;
+            let new_dist_x = (tx as isize - new_mx as isize).abs() as usize;
+            if new_dist_x < dist_x {
+                self.grid.minotaur.move_to(new_mx, my);
+                return;
+            }
         }
 
-        return Ok(Game {
-            grid: Grid {},
-        });
-    }
+        // Check right: if valid and decreases x-distance
+        if mx < self.grid.width - 1 && !self.is_wall(my, mx + 1) {
+            let new_mx = mx + 1;
+            let new_dist_x = (tx as isize - new_mx as isize).abs() as usize;
+            if new_dist_x < dist_x {
+                self.grid.minotaur.move_to(new_mx, my);
+                return;
+            }
+        }
 
-    // TODO
-    pub fn show(&self) {
-    }
+        // Check up: if valid and decreases y-distance
+        if my > 0 && !self.is_wall(my - 1, mx) {
+            let new_my = my - 1;
+            let new_dist_y = (ty as isize - new_my as isize).abs() as usize;
+            if new_dist_y < dist_y {
+                self.grid.minotaur.move_to(mx, new_my);
+                return;
+            }
+        }
 
-    // TODO
-    pub fn minotaur_move(&mut self) {
+        // Check down: if valid and decreases y-distance
+        if my < self.grid.height - 1 && !self.is_wall(my + 1, mx) {
+            let new_my = my + 1;
+            let new_dist_y = (ty as isize - new_my as isize).abs() as usize;
+            if new_dist_y < dist_y {
+                self.grid.minotaur.move_to(mx, new_my);
+            }
+        }
     }
-
-    // TODO
     pub fn theseus_move(&mut self, command: Command) {
+        let current_pos = self.grid.theseus;
+        let mut new_pos = current_pos;
+
+        match command {
+            Command::Up => {
+                if new_pos.y > 0 {
+                    new_pos.y -= 1;
+                }
+            }
+            Command::Down => {
+                if new_pos.y < self.grid.height - 1 {
+                    new_pos.y += 1;
+                }
+            }
+            Command::Left => {
+                if new_pos.x > 0 {
+                    new_pos.x -= 1;
+                }
+            }
+            Command::Right => {
+                if new_pos.x < self.grid.width - 1 {
+                    new_pos.x += 1;
+                }
+            }
+            Command::Skip => {
+            }
+        }
+
+        // Only move Theseus if the new position is not a wall.
+        if !self.is_wall(new_pos.y, new_pos.x) {
+            self.grid.theseus.move_to(new_pos.x, new_pos.y);
+        }
     }
 
-    // TODO: replace the function body with your implementation
     pub fn status(&self) -> GameStatus {
-        GameStatus::Continue
+        if self.grid.theseus == self.grid.goal {
+            GameStatus::Win
+        } else if self.grid.theseus == self.grid.minotaur {
+            GameStatus::Lose
+        } else {
+            GameStatus::Continue
+        }
     }
 }
-
 impl Game {
-    // TODO: replace the function body with your implementation
-    /// Returns true if the given position is Theseus
     pub fn is_theseus(&self, row: usize, col: usize) -> bool {
-        false
+        self.grid.theseus.y == row && self.grid.theseus.x == col
     }
-    // TODO: replace the function body with your implementation
-    /// Returns true if the given position is Minotaur
     pub fn is_minotaur(&self, row: usize, col: usize) -> bool {
-        false
+        self.grid.minotaur.y == row && self.grid.minotaur.x == col
     }
-    // TODO: replace the function body with your implementation
-    /// Returns true if the given position is a wall
     pub fn is_wall(&self, row: usize, col: usize) -> bool {
-        false
+        self.grid.walls.iter().any(|w| w.y == row && w.x == col)
     }
-    // TODO: replace the function body with your implementation
-    /// Returns true if the given position is the goal
     pub fn is_goal(&self, row: usize, col: usize) -> bool {
-        false
+        self.grid.goal.y == row && self.grid.goal.x == col
     }
-    // TODO: replace the function body with your implementation
-    /// Returns true if the given position is empty
     pub fn is_empty(&self, row: usize, col: usize) -> bool {
-        false
+        !self.is_theseus(row, col)
+            && !self.is_minotaur(row, col)
+            && !self.is_goal(row, col)
+            && !self.is_wall(row, col)
     }
 }
 
@@ -202,7 +292,18 @@ pub enum Command {
 //  "a", "s", and "d" to take input, or else the words "up", "down", "left", "right". You can take
 //  input however you like, so long as you document it here in a comment and it is reasonable to
 //  use as a player.
-pub fn input(stdin: impl io::Read + io::BufRead) -> Option<Command> {
-    // TODO: replace this loop with your code
-    loop {}
+pub fn input(mut stdin: impl io::Read + io::BufRead) -> Option<Command> {
+    let mut buffer = String::new();
+    if stdin.read_line(&mut buffer).is_err() {
+        return None;
+    }
+
+    match buffer.trim() {
+        "w" | "up" => Some(Command::Up),
+        "s" | "down" => Some(Command::Down),
+        "a" | "left" => Some(Command::Left),
+        "d" | "right" => Some(Command::Right),
+        " " | "skip" => Some(Command::Skip),
+        _ => None,
+    }
 }
